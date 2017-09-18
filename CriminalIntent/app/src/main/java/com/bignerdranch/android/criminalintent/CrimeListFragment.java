c1435bf88efb79561d9d93ceba4c40ac42accd0b
KranthiChinnakotla <kranthi2@hotmail.com>
2017-09-18 17:19:58 -0400

    modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java 	modified:   app/src/main/res/menu/fragment_crime_list.xml

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
@@ -33,6 +34,9 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mCrimeAdapter;
     private TextView mTextViewTitle, mTextViewDate;
     private ImageView mImageView_Solved;
+    private boolean mSubTitleVisible;
+    private static String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
 
     private static String TAG = "com.bignerdranch.android.crimeid";
 
@@ -42,8 +46,17 @@ public class CrimeListFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        if(savedInstanceState != null){
+            mSubTitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
         updateUi();
         return v;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE,mSubTitleVisible);
     }
 
     @Override
@@ -56,17 +69,29 @@ public class CrimeListFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        MenuItem subTitleItem = menu.findItem(R.id.show_title);
+        if(mSubTitleVisible){
+            subTitleItem.setTitle(getString(R.string.hide_subtitle));
+        }else{
+            subTitleItem.setTitle(getString(R.string.show_subtitle));
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case R.id.new_crime:
                 Crime crime = new Crime();
+                crime.setTitle(getString(R.string.app_name));
                 CrimeLab.getCrimeLab(getActivity()).addCrime(crime);
                 Intent intent = CrimePagerActivity.newIntent(getActivity());
                 startActivity(intent);
+                return true;
+            case R.id.show_title:
+                mSubTitleVisible = !mSubTitleVisible;
+                getActivity().invalidateOptionsMenu();
+                updateTitle();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -148,7 +173,6 @@ public class CrimeListFragment extends Fragment {
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
-
             return new CrimeViewHolder(inflater, parent);
         }
 
@@ -185,13 +209,23 @@ public class CrimeListFragment extends Fragment {
             mCrimeAdapter.notifyDataSetChanged();
             mRecyclerView.setAdapter(mCrimeAdapter);
         }
+        updateTitle();
+    }
 
+    private void updateTitle() {
+        CrimeLab crimeLab = CrimeLab.getCrimeLab(getActivity());
+        int count = crimeLab.getCrimesList().size();
+        String subTitle = getString(R.string.subtitle_format, count);
+        if(!mSubTitleVisible){
+            subTitle = null;
+        }
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setTitle(subTitle);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         updateUi();
-
     }
 }
